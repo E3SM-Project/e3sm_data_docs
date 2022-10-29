1d0dc906aef4cceb9901657c4a1c4e3efaffe1b7
forsyth2 <30700190+forsyth2@users.noreply.github.com>
2022-10-28 17:44:52 -0700

    Add new simulations and NARRM paths (#18)

--- utils/generate_table.py
+++ utils/generate_table.py
@@ -4,12 +4,10 @@ import re
 import requests
 
 class Simulation(object):
-    def __init__(self, name, experiment=None, ensemble_num=None):
+    def __init__(self, name, resolution, experiment=None, ensemble_num=None):
         self.name = name
-        self.experiment = experiment
-        self.ensemble_num = ensemble_num
         
-        hpss_path = f"/home/projects/e3sm/www/WaterCycle/E3SMv2/LR/{name}"
+        hpss_path = f"/home/projects/e3sm/www/WaterCycle/E3SMv2/{resolution}/{name}"
         output = "out.txt"
         if os.path.exists(output):
             os.remove(output)
@@ -75,11 +73,11 @@ def create_simulation_objects():
     deck = Category("DECK")
     low_res.append(deck)
     simulation_tuples = [
-        ("v2.LR.piControl", "piControl", 1),
-        ("v2.LR.piControl_land", None, None),
-        ("v2.LR.abrupt-4xCO2_0101", "abrupt-4xCO2", 1),
-        ("v2.LR.abrupt-4xCO2_0301", "abrupt-4xCO2", 2),
-        ("v2.LR.1pctCO2_0101", "1pctCO2", 1),
+        ("v2.LR.piControl","LR", "piControl", 1),
+        ("v2.LR.piControl_land", "LR", None, None),
+        ("v2.LR.abrupt-4xCO2_0101", "LR", "abrupt-4xCO2", 1),
+        ("v2.LR.abrupt-4xCO2_0301", "LR", "abrupt-4xCO2", 2),
+        ("v2.LR.1pctCO2_0101", "LR", "1pctCO2", 1),
     ]
     for simulation_tuple in simulation_tuples:
         deck.append(Simulation(*simulation_tuple))
@@ -87,57 +85,57 @@ def create_simulation_objects():
     historical = Category("Historical")
     low_res.append(historical)
     simulation_tuples = [
-        ("v2.LR.historical_0101", "historical", 1),
-        ("v2.LR.historical_0151", "historical", 2),
-        ("v2.LR.historical_0201", "historical", 3),
-        ("v2.LR.historical_0251", "historical", 4),
-        ("v2.LR.historical_0301", "historical", 5),
-        ("v2.LR.historical_0101_bonus", None, None),
+        ("v2.LR.historical_0101", "LR", "historical", 1),
+        ("v2.LR.historical_0151", "LR", "historical", 2),
+        ("v2.LR.historical_0201", "LR", "historical", 3),
+        ("v2.LR.historical_0251", "LR", "historical", 4),
+        ("v2.LR.historical_0301", "LR", "historical", 5),
+        ("v2.LR.historical_0101_bonus", "LR", None, None),
     ]
     for simulation_tuple in simulation_tuples:
         historical.append(Simulation(*simulation_tuple))
 
     historical_le = Category("Historical LE")
     low_res.append(historical_le)
-    simulation_names = [
-        "v2.LR.historical_0111",
-        "v2.LR.historical_0121",
-        "v2.LR.historical_0131",
-        "v2.LR.historical_0141",
-        "v2.LR.historical_0161",
-        "v2.LR.historical_0171",
-        "v2.LR.historical_0181",
-        "v2.LR.historical_0191",
-        "v2.LR.historical_0211",
-        "v2.LR.historical_0221",
-        "v2.LR.historical_0231",
-        "v2.LR.historical_0241",
-        "v2.LR.historical_0261",
-        "v2.LR.historical_0271",
-        "v2.LR.historical_0281",
-        "v2.LR.historical_0291",
+    simulation_tuples = [
+        ("v2.LR.historical_0111", "LR"),
+        ("v2.LR.historical_0121", "LR"),
+        ("v2.LR.historical_0131", "LR"),
+        ("v2.LR.historical_0141", "LR"),
+        ("v2.LR.historical_0161", "LR"),
+        ("v2.LR.historical_0171", "LR"),
+        ("v2.LR.historical_0181", "LR"),
+        ("v2.LR.historical_0191", "LR"),
+        ("v2.LR.historical_0211", "LR"),
+        ("v2.LR.historical_0221", "LR"),
+        ("v2.LR.historical_0231", "LR"),
+        ("v2.LR.historical_0241", "LR"),
+        ("v2.LR.historical_0261", "LR"),
+        ("v2.LR.historical_0271", "LR"),
+        ("v2.LR.historical_0281", "LR"),
+        ("v2.LR.historical_0291", "LR"),
     ]
-    for simulation_name in simulation_names:
-        historical_le.append(Simulation(simulation_name))
+    for simulation_tuple in simulation_tuples:
+        historical_le.append(Simulation(*simulation_tuple))
 
     single_forcing = Category("Single-forcing (DAMIP-like)")
     low_res.append(single_forcing)
     simulation_tuples = [
-        ("v2.LR.hist-GHG_0101", "hist-GHG", 1),
-        ("v2.LR.hist-GHG_0151", "hist-GHG", 2),
-        ("v2.LR.hist-GHG_0201", "hist-GHG", 3),
-        ("v2.LR.hist-GHG_0251", "hist-GHG", 4),
-        ("v2.LR.hist-GHG_0301", "hist-GHG", 5),
-        ("v2.LR.hist-aer_0101", "hist-aer", 1),
-        ("v2.LR.hist-aer_0151", "hist-aer", 2),
-        ("v2.LR.hist-aer_0201", "hist-aer", 3),
-        ("v2.LR.hist-aer_0251", "hist-aer", 4),
-        ("v2.LR.hist-aer_0301", "hist-aer", 5),
-        ("v2.LR.hist-all-xGHG-xaer_0101", None, None),
-        ("v2.LR.hist-all-xGHG-xaer_0151", None, None),
-        ("v2.LR.hist-all-xGHG-xaer_0201", None, None),
-        ("v2.LR.hist-all-xGHG-xaer_0251", None, None),
-        ("v2.LR.hist-all-xGHG-xaer_0301", None, None),
+        ("v2.LR.hist-GHG_0101", "LR", "hist-GHG", 1),
+        ("v2.LR.hist-GHG_0151", "LR", "hist-GHG", 2),
+        ("v2.LR.hist-GHG_0201", "LR", "hist-GHG", 3),
+        ("v2.LR.hist-GHG_0251", "LR", "hist-GHG", 4),
+        ("v2.LR.hist-GHG_0301", "LR", "hist-GHG", 5),
+        ("v2.LR.hist-aer_0101", "LR", "hist-aer", 1),
+        ("v2.LR.hist-aer_0151", "LR", "hist-aer", 2),
+        ("v2.LR.hist-aer_0201", "LR", "hist-aer", 3),
+        ("v2.LR.hist-aer_0251", "LR", "hist-aer", 4),
+        ("v2.LR.hist-aer_0301", "LR", "hist-aer", 5),
+        ("v2.LR.hist-all-xGHG-xaer_0101", "LR", None, None),
+        ("v2.LR.hist-all-xGHG-xaer_0151", "LR", None, None),
+        ("v2.LR.hist-all-xGHG-xaer_0201", "LR", None, None),
+        ("v2.LR.hist-all-xGHG-xaer_0251", "LR", None, None),
+        ("v2.LR.hist-all-xGHG-xaer_0301", "LR", None, None),
     ]
     for simulation_tuple in simulation_tuples:
         single_forcing.append(Simulation(*simulation_tuple))
@@ -146,10 +144,10 @@ def create_simulation_objects():
     amip = Category("AMIP")
     low_res.append(amip)
     simulation_tuples = [
-        ("v2.LR.amip_0101", "amip", 1),
-        ("v2.LR.amip_0201", "amip", 2),
-        ("v2.LR.amip_0301", "amip", 3),
-        ("v2.LR.amip_0101_bonus", None, None)
+        ("v2.LR.amip_0101", "LR", "amip", 1),
+        ("v2.LR.amip_0201", "LR", "amip", 2),
+        ("v2.LR.amip_0301", "LR", "amip", 3),
+        ("v2.LR.amip_0101_bonus", "LR", None, None)
     ]
     for simulation_tuple in simulation_tuples:
         amip.append(Simulation(*simulation_tuple))
@@ -157,52 +155,70 @@ def create_simulation_objects():
     rfmip = Category("RFMIP")
     low_res.append(rfmip)
     simulation_tuples = [
-        ("v2.LR.piClim-control", "piClim-control", 1),
-        ("v2.LR.piClim-histall_0021", "piClim-histall", 1),
-        ("v2.LR.piClim-histall_0031", "piClim-histall", 2),
-        ("v2.LR.piClim-histall_0041", "piClim-histall", 3),
-        ("v2.LR.piClim-histaer_0021", "piClim-histaer", 1),
-        ("v2.LR.piClim-histaer_0031", "piClim-histaer", 2),
-        ("v2.LR.piClim-histaer_0041", "piClim-histaer", 3)
+        ("v2.LR.piClim-control", "LR", "piClim-control", 1),
+        ("v2.LR.piClim-histall_0021", "LR", "piClim-histall", 1),
+        ("v2.LR.piClim-histall_0031", "LR", "piClim-histall", 2),
+        ("v2.LR.piClim-histall_0041", "LR", "piClim-histall", 3),
+        ("v2.LR.piClim-histaer_0021", "LR", "piClim-histaer", 1),
+        ("v2.LR.piClim-histaer_0031", "LR", "piClim-histaer", 2),
+        ("v2.LR.piClim-histaer_0041", "LR", "piClim-histaer", 3)
     ]
     for simulation_tuple in simulation_tuples:
         rfmip.append(Simulation(*simulation_tuple))
+
+    other = Category("Other")
+    low_res.append(other)
+    simulation_tuples = [
+        ("v2_ndgclim_t6h_1850aer", "LR"),
+        ("v2_ndgclim_t6h_2010aer", "LR"),
+    ]
+    for simulation_tuple in simulation_tuples:
+        other.append(Simulation(*simulation_tuple))
 
     rrm = Resolution("Water Cycle (NARRM)")
 
     deck = Category("DECK")
     rrm.append(deck)
-    simulation_names = [
-        "v2.NARRM.piControl",
-        "v2.NARRM.abrupt-4xCO2_0101",
-        "v2.NARRM.1pctCO2_0101",
+    simulation_tuples = [
+        ("v2.NARRM.piControl", "NARRM"),
+        ("v2.NARRM.abrupt-4xCO2_0101", "NARRM"),
+        ("v2.NARRM.1pctCO2_0101", "NARRM"),
     ]
-    for simulation_name in simulation_names:
-        deck.append(Simulation(simulation_name))
+    for simulation_tuple in simulation_tuples:
+        deck.append(Simulation(*simulation_tuple))
 
     historical = Category("Historical")
     rrm.append(historical)
-    simulation_names = [
-        "v2.NARRM.historical_0101",
-        "v2.NARRM.historical_0151",
-        "v2.NARRM.historical_0201",
-        "v2.NARRM.historical_0251",
-        "v2.NARRM.historical_0301",
-        "v2.NARRM.historical_0101_bonus",
+    simulation_tuples = [
+        ("v2.NARRM.historical_0101", "NARRM"),
+        ("v2.NARRM.historical_0151", "NARRM"),
+        ("v2.NARRM.historical_0201", "NARRM"),
+        ("v2.NARRM.historical_0251", "NARRM"),
+        ("v2.NARRM.historical_0301", "NARRM"),
+        ("v2.NARRM.historical_0101_bonus", "NARRM"),
     ]
-    for simulation_name in simulation_names:
-        historical.append(Simulation(simulation_name))
+    for simulation_tuple in simulation_tuples:
+        historical.append(Simulation(*simulation_tuple))
 
     amip = Category("AMIP")
     rrm.append(amip)
-    simulation_names = [
-        "v2.NARRM.amip_0101",
-        "v2.NARRM.amip_0201",
-        "v2.NARRM.amip_0301",
-        "v2.NARRM.amip_0101_bonus",
+    simulation_tuples = [
+        ("v2.NARRM.amip_0101", "NARRM"),
+        ("v2.NARRM.amip_0201", "NARRM"),
+        ("v2.NARRM.amip_0301", "NARRM"),
+        ("v2.NARRM.amip_0101_bonus", "NARRM"),
     ]
-    for simulation_name in simulation_names:
-        amip.append(Simulation(simulation_name))
+    for simulation_tuple in simulation_tuples:
+        amip.append(Simulation(*simulation_tuple))
+
+    other = Category("Other")
+    rrm.append(other)
+    simulation_tuples = [
+        ("v2.NA.F20TR.6h.f1.1850aer", "NARRM"),
+        ("v2.NA.F20TR.6h.f1.2010aer", "NARRM"),
+    ]
+    for simulation_tuple in simulation_tuples:
+        other.append(Simulation(*simulation_tuple))
         
     return [low_res, rrm]
 
