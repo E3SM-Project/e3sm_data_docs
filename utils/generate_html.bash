6720d7bef253abcc0df05bd551ecc2eaed901640
AaronDonahue <donahue.aaron@gmail.com>
2024-11-25 17:22:46 -0800

    Update FourSeason run scripts with version information. (#52)
    * Update FourSeason run scripts with version information.
    
    * Add warning about runscripts for SCREAMv0/v1
    
    * Fix typo in Original Run Scripts wording
    
    * Formatting fixes
    
    * Address comments
    
    * Clarify data retrieval procedure for SCREAM output
    
    * Fix typo
    
    * Update generator script
    
    ---------
    
    Co-authored-by: Ryan Forsyth <forsyth2@llnl.gov>
    Co-authored-by: Chris Terai <terai1@llnl.gov>

--- utils/generate_html.bash
+++ utils/generate_html.bash
@@ -1,4 +1,4 @@
-pr_num=50
+pr_num=52
 
 # Chrysalis
 #destination_dir=/lcrc/group/e3sm/public_html/diagnostic_output/$USER/data_docs_${pr_num}
@@ -7,7 +7,7 @@ pr_num=50
 destination_dir=/global/cfs/cdirs/e3sm/www/$USER/data_docs_${pr_num}
 web_page="https://portal.nersc.gov/cfs/e3sm/$USER/data_docs_${pr_num}/html/"
 
-python generate_tables.py
+#python generate_tables.py
 cd ../docs/ && make html
 rm -rf ls ${destination_dir}
 mv _build ${destination_dir}
