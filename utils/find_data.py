f0499ba805f7a4a88ea4a4f3ba678dbdf5a22f77
Ryan Forsyth <forsyth2@llnl.gov>
2025-07-31 17:13:49 -0700

    Add more columns to data-finding table

--- utils/find_data.py
+++ utils/find_data.py
@@ -5,6 +5,44 @@ from typing import List, Tuple
 
 # Run with `time python find_data.py > out_find_data.txt`
 
+# Standard HPSS `/home/projects/e3sm/www/WaterCycle/E3SMv1/` paths from
+# https://docs.e3sm.org/e3sm_data_docs/_build/html/v1/WaterCycle/simulation_data/simulation_table.html
+CENTRALIZED_PREFIXES: List[str] = [
+    "/home/projects/e3sm/www/WaterCycle/E3SMv1/LR/",
+    "/home/projects/e3sm/www/WaterCycle/E3SMv1/HR/"
+]
+# User-specific paths from
+# https://e3sm.atlassian.net/wiki/spaces/ED/pages/4495441922/V1+Simulation+backfill+WIP
+USER_PREFIXES: List[str] = [
+    "/home/b/beharrop/E3SM_simulations/",
+    "/home/c/chengzhu/",
+    "/home/d/dcomeau/cryosphere_simulations/",
+    "/home/g/golaz/2018/E3SM_simulations/",
+    "/home/g/golaz/2018/E3SM_simulations/repaired/",
+    "/home/g/golaz/2019/E3SM_simulations/",
+    "/home/g/golaz/2019/E3SM_simulations/repaired/",
+    "/home/j/jinyun/CBGCv1/",
+    "/home/j/jonbob/",
+    "/home/m/maltrud/E3SM/",
+    "/home/n/ndk/2019/",
+    "/home/n/ndk/2020/",
+    "/home/n/ndk/2021/",
+    "/home/projects/m3412/"
+    "/home/s/shix/E3SM/",
+    "/home/t/tang30/2018/E3SM_simulations/",
+    "/home/t/tang30/2018/E3SM_simulations/repaired/",
+    "/home/t/tang30/2019/E3SM_simulations/",
+    "/home/t/tang30/2019/E3SM_simulations/repaired/",
+    "/home/q/qzhu/CBGCv1/",
+    "/home/z/zshaheen/2018/E3SM_simulations/",
+    "/home/z/zshaheen/2018/E3SM_simulations/repaired/",
+]
+# Publication paths `/home/projects/e3sm/www/publication-archives/` noted in
+# https://github.com/E3SM-Project/e3sm_data_docs/pull/59#issuecomment-3063668732
+PUBLICATION_PREFIXES: List[str] = [
+    "/home/projects/e3sm/www/publication-archives/pub_archive_E3SM_1_0_"
+]
+
 class DataPathsContainer(object):
     def __init__(self, simulation_name: str):
         self.simulation_name: str = simulation_name
@@ -39,55 +77,15 @@ def read_simulations(csv_file: str) -> List[DataPathsContainer]:
     return containers
 
 def search_for_all_appearances(containers: List[DataPathsContainer]):
-    # Try the standard HPSS `/home/projects/e3sm/www/WaterCycle/E3SMv1/` paths from
-    # https://docs.e3sm.org/e3sm_data_docs/_build/html/v1/WaterCycle/simulation_data/simulation_table.html
-    standard_prefixes: List[str] = ["/home/projects/e3sm/www/WaterCycle/E3SMv1/LR/", "/home/projects/e3sm/www/WaterCycle/E3SMv1/HR/"]
-    # Try the user-specific paths from 
-    # https://e3sm.atlassian.net/wiki/spaces/ED/pages/4495441922/V1+Simulation+backfill+WIP
-    user_prefixes: List[str] = [
-        "/home/b/beharrop/E3SM_simulations/",
-        "/home/c/chengzhu/",
-        "/home/d/dcomeau/cryosphere_simulations/",
-        "/home/g/golaz/2018/E3SM_simulations/",
-        "/home/g/golaz/2018/E3SM_simulations/repaired/",
-        "/home/g/golaz/2019/E3SM_simulations/",
-        "/home/g/golaz/2019/E3SM_simulations/repaired/",
-        "/home/j/jinyun/CBGCv1/",
-        "/home/j/jonbob/",
-        "/home/m/maltrud/E3SM/",
-        "/home/n/ndk/2019/",
-        "/home/n/ndk/2020/",
-        "/home/n/ndk/2021/",
-        "/home/projects/m3412/"
-        "/home/s/shix/E3SM/",
-        "/home/t/tang30/2018/E3SM_simulations/",
-        "/home/t/tang30/2018/E3SM_simulations/repaired/",
-        "/home/t/tang30/2019/E3SM_simulations/",
-        "/home/t/tang30/2019/E3SM_simulations/repaired/",
-        "/home/q/qzhu/CBGCv1/",
-        "/home/z/zshaheen/2018/E3SM_simulations/",
-        "/home/z/zshaheen/2018/E3SM_simulations/repaired/",
-    ]
-    # Try the `/home/projects/e3sm/www/publication-archives/` paths noted in
-    # https://github.com/E3SM-Project/e3sm_data_docs/pull/59#issuecomment-3063668732
-    pub_prefixes: List[str] = [
-        "/home/projects/e3sm/www/publication-archives/pub_archive_E3SM_1_0_"
-    ]
-
-    # For testing
-    # containers = containers[15:25]
-
-    num_prefixes: int = len(standard_prefixes) + len (user_prefixes) + len(pub_prefixes)
+    num_prefixes: int = len(CENTRALIZED_PREFIXES) + len (USER_PREFIXES) + len(PUBLICATION_PREFIXES)
     num_simulations: int = len(containers)
     print(f"Trying {num_prefixes} paths for {num_simulations} simulations ({num_prefixes * num_simulations} combinations)")
-    # Trying 23 paths for 91 simulations (2093 combinations)
-
     for container in containers:
-        for prefix in standard_prefixes:
+        for prefix in CENTRALIZED_PREFIXES:
             add_hpss_paths(container, prefix)
-        for prefix in user_prefixes:
+        for prefix in USER_PREFIXES:
             add_hpss_paths(container, prefix)
-        for prefix in pub_prefixes:
+        for prefix in PUBLICATION_PREFIXES:
             add_hpss_paths(container, prefix)
 
 def add_hpss_paths(container: DataPathsContainer, hpss_prefix: str):
@@ -114,13 +112,41 @@ def construct_markdown_table(containers: List[DataPathsContainer]):
     output_file: str = "out_simulation_paths.md"
     with open(output_file, "w") as f:
         f.write("# Data Paths\n\n")
-        f.write("| Simulation name | Real paths | Symlink paths | Num hard copies | Num total copies |\n")
-        f.write("| --- | --- | --- | --- | --- |\n")
+        f.write("| Simulation name | Physically present in this many places | Accessible from this many places | Physically in publication archives? | Accessible from centralized location? | Real paths | Symlink paths |\n")
+        f.write("| --- | --- | --- | --- | --- | --- | --- |\n")
         for container in containers:
+            physically_in_publication_archive: bool = False
+            accessible_from_central_location: bool = False
             symlinks: List[str] = []
             for (src, dst) in container.symlink_paths:
                 symlinks.append(f"{src} -> {dst}")
-            f.write(f"| {container.simulation_name} | {container.real_paths} | {symlinks} | {container.get_num_hard_copies()} | {container.get_num_total_copies()} | \n")
+                if not physically_in_publication_archive:
+                    physically_in_publication_archive = check_for_prefix(dst, PUBLICATION_PREFIXES)
+                if not accessible_from_central_location:
+                    accessible_from_central_location = check_for_prefix(src, CENTRALIZED_PREFIXES)
+            if not physically_in_publication_archive:
+                for path in container.real_paths:
+                    physically_in_publication_archive = check_for_prefix(path, PUBLICATION_PREFIXES)
+                    if physically_in_publication_archive:
+                        break
+            if not accessible_from_central_location:
+                for path in container.real_paths:
+                    accessible_from_central_location = check_for_prefix(path, CENTRALIZED_PREFIXES)
+                    if accessible_from_central_location:
+                        break
+            f.write(f"| {container.simulation_name} | {container.get_num_hard_copies()} | {container.get_num_total_copies()} | {bool2str(physically_in_publication_archive)} | {bool2str(accessible_from_central_location)} | {container.real_paths} | {symlinks} |\n")
+
+def check_for_prefix(path: str, prefix_list: List[str]):
+    for prefix in prefix_list:
+        if path.startswith(prefix):
+            return True
+    return False
+
+def bool2str(val: bool) -> str:
+    if val:
+        return "Yes"
+    else:
+        return "" # Empty string
 
 if __name__ == "__main__":
     containers: List[DataPathsContainer] = read_simulations("input/simulations_v1_water_cycle.csv")
